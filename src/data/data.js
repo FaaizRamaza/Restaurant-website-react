a1e41251154eda4ecd62b33b87c7a25d6c8e8d89
FaaizRamaza <faizputra991@gmail.com>
2023-06-06 19:01:45 +0700

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,202 @@
+export const topPicks = [
+  {
+    id: 1,
+    title: "Shawara Chicken",
+    price: "$350",
+    img: "https://images.squarespace-cdn.com/content/v1/5602c3cce4b0f8716df3f66d/1545303032172-OKSDH1PWM0WD8HNYZRUV/Online+Personal+Trainer+Lunch+Recipes+-%C2%A0Chicken+Shawara+Wrap....jpg",
+  },
+  {
+    id: 2,
+    title: "Orange Juice",
+    price: "$350",
+    img: "https://images.healthshots.com/healthshots/en/uploads/2022/02/08134710/JUICES-1600x900.jpg",
+  },
+  {
+    id: 3,
+    title: "Pizza",
+    price: "$350",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Pizza-3007395.jpg/800px-Pizza-3007395.jpg",
+  },
+  {
+    id: 4,
+    title: "Pepperoni Pizza",
+    price: "$350",
+    img: "https://www.allrecipes.com/thmb/0xH8n2D4cC97t7mcC7eT2SDZ0aE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/6776_Pizza-Dough_ddmfs_2x1_1725-fdaa76496da045b3bdaadcec6d4c5398.jpg",
+  },
+  {
+    id: 5,
+    title: "Chicken Wings",
+    price: "$350",
+    img: "https://tastesbetterfromscratch.com/wp-content/uploads/2014/09/Baked-Chicken-Wings-3.jpg",
+  },
+  {
+    id: 6,
+    title: "Fruit Juice",
+    price: "$350",
+    img: "https://www.modernhoney.com/wp-content/uploads/2018/09/Healthy-Juice-Cleanse-Recipes-1.jpg",
+  },
+  {
+    id: 7,
+    title: "Cheese Burger",
+    price: "$350",
+    img: "https://img.kurio.network/QxKbt870DH97dKwzrHdsVy2S3Lw=/1200x1200/filters:quality(80)/https://kurio-img.kurioapps.com/21/09/01/66145feb-79af-4e77-ac79-32af2f70faee.jpe",
+  },
+  {
+    id: 8,
+    title: "Black Burger",
+    price: "$350",
+    img: "https://img.qraved.co/v2/image/data/black-burger-1520218555662-x.jpeg",
+  },
+  {
+    id: 9,
+    title: "Cheese Salad",
+    price: "$350",
+    img: "https://www.acouplecooks.com/wp-content/uploads/2019/05/Chopped-Salad-001_1.jpg",
+  },
+  {
+    id: 10,
+    title: "Fruit Salad",
+    price: "$350",
+    img: "https://www.wholesomeyum.com/wp-content/uploads/2020/06/wholesomeyum-Chef-Salad-Recipe-16-500x500.jpg",
+  }
+];
+
+export const mealData = [
+  {
+    id: 1,
+    name: "Cheese Pizza",
+    category: "pizza",
+    price: "$350",
+    img: "https://static.toiimg.com/thumb/53110049.cms?width=1200&height=900",
+  },
+  {
+    id: 1,
+    name: "Pepperoni Pizza",
+    category: "pizza",
+    price: "$350",
+    img: "https://www.allrecipes.com/thmb/0xH8n2D4cC97t7mcC7eT2SDZ0aE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/6776_Pizza-Dough_ddmfs_2x1_1725-fdaa76496da045b3bdaadcec6d4c5398.jpg",
+  },{
+    id: 1,
+    name: "Veggie Pizza",
+    category: "pizza",
+    price: "$350",
+    img: "https://cookieandkate.com/images/2021/07/classic-margherita-pizza.jpg",
+  },{
+    id: 1,
+    name: "Classic Pizza",
+    category: "pizza",
+    price: "$350",
+    img: "https://static.toiimg.com/photo/56868564.cms",
+  },{
+    id: 1,
+    name: "Fried Chicken",
+    category: "chicken",
+    price: "$350",
+    img: "https://www.seriouseats.com/thmb/xw1krLC9Yh85qx1wl5jw0BPCWHk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__2015__07__20210324-SouthernFriedChicken-Andrew-Janjigian-21-cea1fe39234844638018b15259cabdc2.jpg",
+  },{
+    id: 1,
+    name: "Roasted Chicken",
+    category: "chicken",
+    price: "$350",
+    img: "https://hips.hearstapps.com/hmg-prod/images/190410-oven-barbecue-chicken-369-1555947776.jpg?crop=1xw:0.8435280189423836xh;center,top",
+  },{
+    id: 1,
+    name: "Chicken BBQ",
+    category: "chicken",
+    price: "$350",
+    img: "https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2Farchive%2F20b793e3b4157385feb19a6ec6a3a04dfc2b46f4",
+  },{
+    id: 1,
+    name: "Chicken Original",
+    category: "chicken",
+    price: "$350",
+    img: "https://www.themediterraneandish.com/wp-content/uploads/2020/07/grilled-whole-chicken-recipe-10.jpg",
+  },{
+    id: 1,
+    name: "Caesar Salad",
+    category: "salad",
+    price: "$350",
+    img: "https://www.wholesomeyum.com/wp-content/uploads/2020/06/wholesomeyum-Chef-Salad-Recipe-16-500x500.jpg",
+  },{
+    id: 1,
+    name: "Loaded Salad",
+    category: "salad",
+    price: "$350",
+    img: "https://static01.nyt.com/images/2022/07/15/dining/MC-Chopped-Salad-15SALADREX/merlin_209652387_1b5eee4c-9da5-443c-90e0-db20ee51a246-master768.jpg",
+  },{
+    id: 1,
+    name: "Kale Salad",
+    category: "salad",
+    price: "$350",
+    img: "https://natashaskitchen.com/wp-content/uploads/2019/01/Caesar-Salad-Recipe-3.jpg",
+  },{
+    id: 1,
+    name: "American Salad",
+    category: "salad",
+    price: "$350",
+    img: "https://natashaskitchen.com/wp-content/uploads/2019/01/Caesar-Salad-Recipe-3.jpg",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    name: "Veg",
+    img: "https://mudaberdaya.id/wp-content/uploads/2021/06/garuda-food-logo.png",
+  },
+  {
+    id: 2,
+    title: "Burger",
+    img: "https://i.pinimg.com/originals/42/ce/a8/42cea81f223233ab3d75b5c51261bd79.png",
+  },
+  {
+    id: 3,
+    title: "Pizza",
+    img: "https://download.logo.wine/logo/Foodpanda/Foodpanda-Logo.wine.png",
+  },
+  {
+    id: 4,
+    title: "Wings",
+    img: "https://gfi.org/wp-content/uploads/2021/09/Founding_Beyond-1-1024x420.png",
+  },
+  {
+    id: 5,
+    title: "Shamarwa",
+    img: "https://gfi.org/wp-content/uploads/2021/09/Founding_Laurus-1-1024x291.png",
+  },
+  {
+    id: 6,
+    name: "Burger",
+    img: "https://gfi.org/wp-content/uploads/2021/09/Founding_Griffith-1-1024x150.png",
+  },
+  {
+    id: 7,
+    title: "Veg",
+    img: "https://gfi.org/wp-content/uploads/2021/09/Founding_PowerPlant-1-1024x283.png",
+  },
+  {
+    id: 8,
+    title: "Pizza",
+    img: "https://gfi.org/wp-content/uploads/2021/09/Founding_Big-Idea-Ventures-1-1024x333.png",
+  },
+  {
+    id: 9,
+    title: "Wings",
+    img: "https://gfi.org/wp-content/uploads/2021/09/Founding_CIBO-1-1024x278.png",
+  },
+  {
+    id: 10,
+    title: "Shamarwa",
+    img: "https://gfi.org/wp-content/uploads/2021/09/Founding_Givaudan-1-1024x404.png",
+  },
+  {
+    id: 11,
+    name: "Burger",
+    img: "https://www.pngmart.com/files/16/Sponsor-PNG-File.png",
+  },
+  {
+    id: 12,
+    title: "Veg",
+    img: "https://amralive.com/wp-content/uploads/2019/01/sponsor4.png",
+  }
+];
